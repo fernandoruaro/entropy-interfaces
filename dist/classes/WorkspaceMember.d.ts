1cfe6e02d86afac261a8595c9e6bf7e24ddacc15
Fernando Ruaro <landell.ruaro@gmail.com>
2018-09-16 20:16:19 -0300

    Adding Role

--- dist/classes/WorkspaceMember.d.ts
+++ dist/classes/WorkspaceMember.d.ts
@@ -2,4 +2,5 @@ import { Deletable } from './Deletable';
 export declare class WorkspaceMember extends Deletable {
     userEmail: string;
     workspaceId: string;
+    roles: Array<string>;
 }
--- src/classes/WorkspaceMember.ts
+++ src/classes/WorkspaceMember.ts
@@ -3,4 +3,5 @@ import { Deletable } from './Deletable';
 export class WorkspaceMember extends Deletable {
   userEmail: string
   workspaceId: string
+  roles: Array<string>
 }
