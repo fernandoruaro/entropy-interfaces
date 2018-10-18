1e49bab3616010f52d09b0ea15dd129373789d40
Fernando Ruaro <landell.ruaro@gmail.com>
2018-10-18 02:53:29 -0300

    Adding user to workspace member

--- dist/classes/WorkspaceMember.d.ts
+++ dist/classes/WorkspaceMember.d.ts
@@ -1,6 +1,8 @@
 import { Deletable } from './Deletable';
+import { User } from './User';
 export interface WorkspaceMember extends Deletable {
     userEmail: string;
     workspaceId: string;
     roles: Array<string>;
+    user?: User;
 }
--- src/classes/WorkspaceMember.ts
+++ src/classes/WorkspaceMember.ts
@@ -1,7 +1,9 @@
 import { Deletable } from './Deletable';
+import { User } from './User';
 
 export interface WorkspaceMember extends Deletable {
   userEmail: string
   workspaceId: string
   roles: Array<string>
+  user?: User
 }
