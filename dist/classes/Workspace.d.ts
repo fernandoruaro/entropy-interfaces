15938f2aa05f1de73bdd9434812ffd2bc0d34fae
Fernando Ruaro <landell.ruaro@gmail.com>
2018-10-17 21:28:55 -0300

    Adding workspace layout

--- dist/classes/Workspace.d.ts
+++ dist/classes/Workspace.d.ts
@@ -1,6 +1,10 @@
 import { Deletable } from './Deletable';
+export interface WorkspaceLayout {
+    mainColor: string;
+}
 export interface Workspace extends Deletable {
     id?: string;
     name: string;
     subdomain: string;
+    layout: WorkspaceLayout;
 }
--- src/classes/Workspace.ts
+++ src/classes/Workspace.ts
@@ -1,7 +1,12 @@
 import { Deletable } from './Deletable';
 
+export interface WorkspaceLayout {
+  mainColor: string
+}
+
 export interface Workspace extends Deletable {
   id?: string
   name: string
   subdomain: string
+  layout: WorkspaceLayout
 }
