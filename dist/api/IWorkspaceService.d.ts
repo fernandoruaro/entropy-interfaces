55b128a1728e28bf621786b66d7d56dc3bcac47e
Fernando Ruaro <landell.ruaro@gmail.com>
2019-01-18 20:50:40 -0200

    Adding subdomain endpoint

--- dist/api/IWorkspaceService.d.ts
+++ dist/api/IWorkspaceService.d.ts
@@ -1,5 +1,6 @@
 import { Workspace } from "../classes/Workspace";
 export declare abstract class IWorkspaceService {
     abstract get(id: string): Promise<Workspace>;
+    abstract getBySubdomain(id: string): Promise<Workspace>;
     abstract createOrUpdate(workspace: Workspace): Promise<Workspace>;
 }
--- src/api/IWorkspaceService.ts
+++ src/api/IWorkspaceService.ts
@@ -2,5 +2,6 @@ import { Workspace } from "../classes/Workspace";
 
 export abstract class IWorkspaceService {
     abstract get(id: string): Promise<Workspace>;
+    abstract getBySubdomain(id: string): Promise<Workspace>;
     abstract createOrUpdate(workspace: Workspace): Promise<Workspace>;
 }
