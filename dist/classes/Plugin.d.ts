994abf6b6f50aed0fc3cd5b87eaa7f398f18f63b
Fernando Ruaro <landell.ruaro@gmail.com>
2019-01-18 01:43:13 -0200

    Adding workspaceIf to plugin

--- dist/classes/Plugin.d.ts
+++ dist/classes/Plugin.d.ts
@@ -1,6 +1,7 @@
 import { Deletable } from "./Deletable";
 export interface Plugin extends Deletable {
     id?: string;
+    workspaceId: string;
     name: string;
     icon: string;
     moduleUrl: string;
