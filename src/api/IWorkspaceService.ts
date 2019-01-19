9841ece0e0677717c1c276eac32aa03d60f6087e
Fernando Ruaro <landell.ruaro@gmail.com>
2019-01-18 23:36:23 -0200

    Workspace get optional parameter

--- src/api/IWorkspaceService.ts
+++ src/api/IWorkspaceService.ts
@@ -1,7 +1,7 @@
 import { Workspace } from "../classes/Workspace";
 
 export abstract class IWorkspaceService {
-    abstract get(id: string): Promise<Workspace>;
+    abstract get(id?: string): Promise<Workspace>;
     abstract getBySubdomain(id: string): Promise<Workspace>;
     abstract createOrUpdate(workspace: Workspace): Promise<Workspace>;
 }
