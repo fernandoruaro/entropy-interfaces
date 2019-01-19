0f051da13604f9b54086f6c2902f56b065f3e09c
Fernando Ruaro <landell.ruaro@gmail.com>
2019-01-19 17:40:34 -0200

    Adjusting IEntityService

--- dist/api/IEntityService.d.ts
+++ dist/api/IEntityService.d.ts
@@ -1,6 +1,7 @@
-import { Subscribable } from "rxjs";
 import { Entity } from "../classes/Entity";
 export declare abstract class IEntityService {
-    abstract list(): Subscribable<Array<Entity>>;
-    abstract get(entityId: any): Subscribable<Entity>;
+    abstract list(): Promise<Array<Entity>>;
+    abstract get(entityId: any): Promise<Entity>;
+    abstract createOrUpdate(entity: Entity): Promise<Entity>;
+    abstract delete(id: string): Promise<void>;
 }
--- src/api/IEntityService.ts
+++ src/api/IEntityService.ts
@@ -1,7 +1,8 @@
-import { Subscribable } from "rxjs";
 import { Entity } from "../classes/Entity";
 
 export abstract class IEntityService {
-    abstract list(): Subscribable<Array<Entity>>;
-    abstract get(entityId): Subscribable<Entity>;
+    abstract list(): Promise<Array<Entity>>;
+    abstract get(entityId): Promise<Entity>;
+    abstract createOrUpdate(entity: Entity): Promise<Entity>;
+    abstract delete(id: string): Promise<void>;
 }
